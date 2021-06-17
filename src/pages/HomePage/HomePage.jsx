48f9c271219c9614061ef5f3f54abb972840b1da
Ivan Jovanovic <mrtanguero@gmail.com>
2021-06-17 20:02:26 +0200

    Add client's home page basic functionalities

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,52 +1,89 @@
-import { Button, Form } from 'antd';
+import { Card, Collapse } from 'antd';
 import React, { useContext, useState } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import DebounceSelect from '../../components/DebounceSelect/DebounceSelect';
+import { useQuery } from 'react-query';
 import authContext from '../../context/authContext';
-import { getClients } from '../../services/clients';
+import { getReservations } from '../../services/reservations';
+
+const { Panel } = Collapse;
 
 export default function HomePage() {
   const auth = useContext(authContext);
-  // const [value, setValue] = useState([]);
+  const [reservations, setReservations] = useState([]);
 
-  const {
-    control,
-    formState: { errors },
-    handleSubmit,
-  } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
+  useQuery('getUserReservations', getReservations, {
+    onSuccess: ({ data: { data } }) => {
+      setReservations(data);
+    },
+    enabled: auth?.user?.roleId === 2,
+  });
 
   return (
     <>
-      <div>Dobrodošli u aplikaciju {auth?.user?.name}!</div>
-      <Form layout="vertical" onSubmitCapture={handleSubmit(onSubmit)}>
-        <Form.Item label="Klijent">
-          <Controller
-            name="client"
-            control={control}
-            render={({ field }) => {
-              return (
-                <DebounceSelect
-                  // value={value}
-                  {...field}
-                  placeholder="Odaberite klijenta"
-                  fetchOptions={getClients}
-                  // onChange={(newValue) => {
-                  //   setValue(newValue);
-                  // }}
-                  style={{
-                    width: '200px',
-                  }}
-                />
-              );
-            }}
-          />
-          <Button htmlType="submit">Submit</Button>
-        </Form.Item>
-      </Form>
+      {auth?.user?.roleId === 1 && (
+        <div>Dobrodošli u aplikaciju {auth?.user?.name}!</div>
+      )}
+      {auth?.user?.roleId === 2 && (
+        <>
+          <Collapse accordion defaultActiveKey={['1']}>
+            <Panel header="Buduće rezervacije" key="1">
+              {reservations
+                .filter(
+                  (reservation) =>
+                    new Date(reservation.from_date).getTime() >
+                    new Date().getTime()
+                )
+                .map((reservation) => {
+                  return (
+                    <Card style={{ width: 300 }} key={reservation.id}>
+                      <p>{reservation.vehicle.plate_no}</p>
+                      <p>
+                        {reservation.from_date}-{reservation.to_date}
+                      </p>
+                    </Card>
+                  );
+                })}
+            </Panel>
+            <Panel header="Rezervacije u toku" key="2">
+              {reservations
+                .filter(
+                  (reservation) =>
+                    new Date(reservation.from_date).getTime() <
+                      new Date().getTime() &&
+                    new Date(reservation.to_date).getTime() >
+                      new Date().getTime()
+                )
+                .map((reservation) => {
+                  return (
+                    <Card style={{ width: 300 }}>
+                      <p>{reservation.vehicle.plate_no}</p>
+                      <p>
+                        {reservation.from_date}-{reservation.to_date}
+                      </p>
+                    </Card>
+                  );
+                })}
+            </Panel>
+            <Panel header="Prošle rezervacije" key="3">
+              {reservations
+                .filter(
+                  (reservation) =>
+                    new Date(reservation.to_date).getTime() <
+                    new Date().getTime()
+                )
+                .map((reservation) => {
+                  return (
+                    <Card style={{ width: 300 }}>
+                      <p>{reservation.vehicle.plate_no}</p>
+                      <p>
+                        {reservation.from_date}-{reservation.to_date}
+                      </p>
+                    </Card>
+                  );
+                })}
+            </Panel>
+          </Collapse>
+        </>
+      )}
     </>
   );
 }
