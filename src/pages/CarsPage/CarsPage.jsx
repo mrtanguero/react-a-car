c94f3d2b463b3ceb6bdfa19b61a1a39db570c90d
Ivan Jovanovic <mrtanguero@gmail.com>
2021-06-22 04:11:02 +0200

    Add different tables formating for small screens

--- src/pages/CarsPage/CarsPage.jsx
+++ src/pages/CarsPage/CarsPage.jsx
@@ -5,10 +5,12 @@ import {
   PageHeader,
   Space,
   Spin,
+  Grid,
   Table,
   Modal,
   message,
   Input,
+  Descriptions,
 } from 'antd';
 import { useInfiniteQuery, useMutation, useQueryClient } from 'react-query';
 import { useTranslation } from 'react-i18next';
@@ -23,10 +25,10 @@ import { deleteVehicle, getVehicles } from '../../services/cars';
 import useIntersectionObserver from '../../hooks/useIntersectionObserver';
 import { currentTotalLength } from '../../helper/functions';
 import MultiStepForm from '../../components/MultiStepForm/MultiStepForm';
-// import Input from 'react-select/src/components/Input';
 
 const { confirm } = Modal;
 const { Search } = Input;
+const { useBreakpoint } = Grid;
 
 export default function CarsPage() {
   const modalCtx = useContext(modalContext);
@@ -35,6 +37,7 @@ export default function CarsPage() {
   );
   const [searchTerm, setSearchTerm] = useState('');
   const { t } = useTranslation();
+  const screens = useBreakpoint();
   const queryClient = useQueryClient();
   const mutation = useMutation((id) => deleteVehicle(id), {
     onSuccess: () => {
@@ -148,98 +151,135 @@ export default function CarsPage() {
           </>
         }
       />
-      <Card>
+      {!screens.xs ? (
+        <Card>
+          <Table
+            loading={!response?.pages.length && isFetching}
+            columns={[
+              {
+                title: t('tableHeaders.plateNo'),
+                dataIndex: 'plate_no',
+                key: 'plates',
+                width: 105,
+              },
+              {
+                title: t('tableHeaders.year'),
+                dataIndex: 'production_year',
+                key: 'year',
+                width: 80,
+              },
+              {
+                title: t('tableHeaders.carType'),
+                dataIndex: ['car_type', 'name'],
+                key: 'type',
+                width: 120,
+              },
+              {
+                title: t('tableHeaders.seatsNum'),
+                dataIndex: 'no_of_seats',
+                key: 'seats',
+                width: 80,
+              },
+              {
+                title: t('tableHeaders.pricePerDay'),
+                dataIndex: 'price_per_day',
+                key: 'price',
+                width: 120,
+              },
+              {
+                title: t('tableHeaders.remarks'),
+                dataIndex: 'remarks',
+                key: 'remarks',
+                width: 200,
+                ellipsis: true,
+                render: (_, record) => {
+                  if (record.remarks) {
+                    return record.remarks;
+                  }
+                  return '-';
+                },
+              },
+              {
+                title: t('tableHeaders.actions'),
+                key: 'action',
+                align: 'center',
+                width: 100,
+                fixed: 'right',
+                render: (text, record) => (
+                  <Space size="small">
+                    <Button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleEditVehicleClick(record.id);
+                      }}
+                      icon={<EditOutlined />}
+                    />
+                    <Button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        confirm({
+                          title: t('modals.confirmVehicleDelete'),
+                          icon: <ExclamationCircleOutlined />,
+                          content: t('modals.actionPermanent'),
+                          okType: 'danger',
+                          onOk() {
+                            return mutation.mutateAsync(record.id);
+                          },
+                          onCancel() {},
+                        });
+                      }}
+                      icon={<DeleteOutlined />}
+                      danger
+                    />
+                  </Space>
+                ),
+              },
+            ]}
+            dataSource={
+              [].concat.apply(
+                [],
+                response?.pages.map((page) => page.data.data)
+              ) || []
+            }
+            rowKey={(record) => record.id}
+            pagination={false}
+            scroll={{ x: '100%', y: '60vh' }}
+            size="small"
+            onRow={(record, index) => {
+              return {
+                onClick: () => {
+                  handleShowVehicle(record.id);
+                },
+                ref:
+                  index === currentTotalLength(response.pages) - 3
+                    ? (node) => setIntersectionObserverTarget(node)
+                    : null,
+              };
+            }}
+          />
+          <div
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              width: '100%',
+              textAlign: 'center',
+            }}
+          >
+            {isFetchingNextPage && <Spin size="small" />}
+          </div>
+        </Card>
+      ) : (
         <Table
           loading={!response?.pages.length && isFetching}
-          columns={[
-            {
-              title: t('tableHeaders.plateNo'),
-              dataIndex: 'plate_no',
-              key: 'plates',
-              width: 105,
-            },
-            {
-              title: t('tableHeaders.year'),
-              dataIndex: 'production_year',
-              key: 'year',
-              width: 80,
-            },
-            {
-              title: t('tableHeaders.carType'),
-              dataIndex: ['car_type', 'name'],
-              key: 'type',
-              width: 120,
-            },
-            {
-              title: t('tableHeaders.seatsNum'),
-              dataIndex: 'no_of_seats',
-              key: 'seats',
-              width: 80,
-            },
-            {
-              title: t('tableHeaders.pricePerDay'),
-              dataIndex: 'price_per_day',
-              key: 'price',
-              width: 120,
-            },
-            {
-              title: t('tableHeaders.remarks'),
-              dataIndex: 'remarks',
-              key: 'remarks',
-              width: 200,
-              ellipsis: true,
-              render: (_, record) => {
-                if (record.remarks) {
-                  return record.remarks;
-                }
-                return '-';
-              },
-            },
-            {
-              title: t('tableHeaders.actions'),
-              key: 'action',
-              align: 'center',
-              width: 100,
-              fixed: 'right',
-              render: (text, record) => (
-                <Space size="small">
-                  <Button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      handleEditVehicleClick(record.id);
-                    }}
-                    icon={<EditOutlined />}
-                  />
-                  <Button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      confirm({
-                        title: t('modals.confirmVehicleDelete'),
-                        icon: <ExclamationCircleOutlined />,
-                        content: t('modals.actionPermanent'),
-                        okType: 'danger',
-                        onOk() {
-                          return mutation.mutateAsync(record.id);
-                        },
-                        onCancel() {},
-                      });
-                    }}
-                    icon={<DeleteOutlined />}
-                    danger
-                  />
-                </Space>
-              ),
-            },
-          ]}
           dataSource={
             [].concat.apply(
               [],
               response?.pages.map((page) => page.data.data)
             ) || []
           }
+          showHeader={false}
           rowKey={(record) => record.id}
           pagination={false}
-          scroll={{ x: '100%', y: '60vh' }}
           size="small"
           onRow={(record, index) => {
             return {
@@ -252,18 +292,78 @@ export default function CarsPage() {
                   : null,
             };
           }}
+          columns={[
+            {
+              title: t('tableHeaders.plateNo'),
+              dataIndex: 'plate_no',
+              key: 'plate',
+              width: '100%',
+              render: (_, record) => {
+                return (
+                  <>
+                    <Descriptions
+                      className="small-screen-table-descriptions"
+                      bordered
+                      size="small"
+                      column={1}
+                      labelStyle={{ width: '50%', textAlign: 'right' }}
+                      contentStyle={{ color: 'grey' }}
+                    >
+                      <Descriptions.Item label={t('formLabels.plateNo')}>
+                        {record.plate_no}
+                      </Descriptions.Item>
+                      <Descriptions.Item label={t('formLabels.year')}>
+                        {record.production_year}
+                      </Descriptions.Item>
+                      <Descriptions.Item label={t('formLabels.carType')}>
+                        {record.car_type.name}
+                      </Descriptions.Item>
+                      <Descriptions.Item label={t('formLabels.seatsNo')}>
+                        {record.no_of_seats}
+                      </Descriptions.Item>
+                      <Descriptions.Item label={t('formLabels.pricePerDay')}>
+                        {record.price_per_day}€
+                      </Descriptions.Item>
+                    </Descriptions>
+                    <Space size="small" className="small-screen-table-buttons">
+                      <Button
+                        className="small-screen-table-button"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleEditVehicleClick(record.id);
+                        }}
+                        icon={<EditOutlined />}
+                      >
+                        Edit
+                      </Button>
+                      <Button
+                        className="small-screen-table-button"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          confirm({
+                            title: t('modals.confirmVehicleDelete'),
+                            icon: <ExclamationCircleOutlined />,
+                            content: t('modals.actionPermanent'),
+                            okType: 'danger',
+                            onOk() {
+                              return mutation.mutateAsync(record.id);
+                            },
+                            onCancel() {},
+                          });
+                        }}
+                        icon={<DeleteOutlined />}
+                        danger
+                      >
+                        Delete
+                      </Button>
+                    </Space>
+                  </>
+                );
+              },
+            },
+          ]}
         />
-        <div
-          style={{
-            position: 'absolute',
-            bottom: 0,
-            width: '100%',
-            textAlign: 'center',
-          }}
-        >
-          {isFetchingNextPage && <Spin size="small" />}
-        </div>
-      </Card>
+      )}
     </>
   );
 }
--- src/pages/ClientsPage/ClientsPage.jsx
+++ src/pages/ClientsPage/ClientsPage.jsx
@@ -5,10 +5,12 @@ import {
   PageHeader,
   Space,
   Spin,
+  Grid,
   Table,
   Modal,
   message,
   Input,
+  Descriptions,
 } from 'antd';
 import { useInfiniteQuery, useMutation, useQueryClient } from 'react-query';
 import { useTranslation } from 'react-i18next';
@@ -27,6 +29,7 @@ import ClientForm from '../../components/ClientForm/ClientForm';
 
 const { confirm } = Modal;
 const { Search } = Input;
+const { useBreakpoint } = Grid;
 
 export default function ClientsPage() {
   const modalCtx = useContext(modalContext);
@@ -35,6 +38,7 @@ export default function ClientsPage() {
     null
   );
   const { t, i18n } = useTranslation();
+  const screens = useBreakpoint();
   const queryClient = useQueryClient();
   const mutation = useMutation((id) => deleteClient(id), {
     onSuccess: () => {
@@ -145,137 +149,180 @@ export default function ClientsPage() {
           </>
         }
       />
-      <Card>
+      {!screens.xs ? (
+        <Card>
+          <Table
+            loading={!response?.pages.length && isFetching}
+            columns={[
+              {
+                title: t('tableHeaders.clientName'),
+                dataIndex: 'name',
+                key: 'name',
+                width: 180,
+              },
+              {
+                title: t('tableHeaders.idDocNum'),
+                dataIndex: 'identification_document_no',
+                key: 'document-id',
+                width: 120,
+                ellipsis: true,
+              },
+              {
+                title: t('tableHeaders.phone'),
+                dataIndex: 'phone_no',
+                key: 'phone',
+                width: 150,
+              },
+              {
+                title: t('tableHeaders.email'),
+                dataIndex: 'email',
+                key: 'email',
+                width: 200,
+              },
+              {
+                title: t('tableHeaders.firstReservationDate'),
+                dataIndex: 'date_of_first_reservation',
+                key: 'first-reservation',
+                width: 100,
+                render: (_, record) => {
+                  if (record.date_of_first_reservation) {
+                    return moment(record.date_of_first_reservation).format(
+                      i18n.language === 'me' ? 'DD.MM.YYYY.' : 'YYYY-MM-DD'
+                    );
+                  }
+                  return '-';
+                },
+                ellipsis: true,
+              },
+              {
+                title: t('tableHeaders.lastReservationDate'),
+                dataIndex: 'date_of_last_reservation',
+                key: 'last-reservation',
+                width: 110,
+                render: (_, record) => {
+                  if (record.date_of_last_reservation) {
+                    return moment(record.date_of_last_reservation).format(
+                      i18n.language === 'me' ? 'DD.MM.YYYY.' : 'YYYY-MM-DD'
+                    );
+                  }
+                  return '-';
+                },
+                ellipsis: true,
+              },
+              {
+                title: t('tableHeaders.remarks'),
+                dataIndex: 'remarks',
+                key: 'remarks',
+                width: 200,
+                ellipsis: true,
+                render: (_, record) => {
+                  if (record.remarks) {
+                    return record.remarks;
+                  }
+                  return '-';
+                },
+              },
+              {
+                title: t('tableHeaders.actions'),
+                key: 'action',
+                align: 'center',
+                width: 100,
+                fixed: 'right',
+                render: (text, record) => (
+                  <Space size="small">
+                    <Button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        if (!record?.user?.id) {
+                          message.error(
+                            'Ovaj klijent je seedovan i nema odgovarajućeg usera'
+                          );
+                          return;
+                        }
+                        handleEditClientClick(record.user.id);
+                      }}
+                      icon={<EditOutlined />}
+                    />
+                    <Button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        if (!record?.user?.id) {
+                          message.error(
+                            'Ovaj klijent je seedovan i nema odgovarajućeg usera'
+                          );
+                          return;
+                        }
+                        confirm({
+                          title: t('modals.confirmClientDelete'),
+                          icon: <ExclamationCircleOutlined />,
+                          content: t('modals.actionPermanentClient'),
+                          okType: 'danger',
+                          onOk() {
+                            return mutation.mutateAsync(
+                              record.user && record.user.id
+                            );
+                          },
+                          onCancel() {},
+                        });
+                      }}
+                      icon={<DeleteOutlined />}
+                      danger
+                    />
+                  </Space>
+                ),
+              },
+            ]}
+            dataSource={
+              [].concat.apply(
+                [],
+                response?.pages.map((page) => page.data.data)
+              ) || []
+            }
+            rowKey={(record) => record.id}
+            pagination={false}
+            scroll={{ x: '100%', y: '60vh' }}
+            size="small"
+            onRow={(record, index) => {
+              return {
+                onClick: () => {
+                  if (!record?.user?.id) {
+                    message.error(
+                      'Ovaj klijent je seedovan i nema odgovarajućeg usera'
+                    );
+                    return;
+                  }
+                  handleShowClient(record.user.id);
+                },
+                ref:
+                  index === currentTotalLength(response.pages) - 3
+                    ? (node) => setIntersectionObserverTarget(node)
+                    : null,
+              };
+            }}
+          />
+          <div
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              width: '100%',
+              textAlign: 'center',
+            }}
+          >
+            {isFetchingNextPage && <Spin size="small" />}
+          </div>
+        </Card>
+      ) : (
         <Table
           loading={!response?.pages.length && isFetching}
-          columns={[
-            {
-              title: t('tableHeaders.clientName'),
-              dataIndex: 'name',
-              key: 'name',
-              width: 180,
-            },
-            {
-              title: t('tableHeaders.idDocNum'),
-              dataIndex: 'identification_document_no',
-              key: 'document-id',
-              width: 120,
-              ellipsis: true,
-            },
-            {
-              title: t('tableHeaders.phone'),
-              dataIndex: 'phone_no',
-              key: 'phone',
-              width: 150,
-            },
-            {
-              title: t('tableHeaders.email'),
-              dataIndex: 'email',
-              key: 'email',
-              width: 200,
-            },
-            {
-              title: t('tableHeaders.firstReservationDate'),
-              dataIndex: 'date_of_first_reservation',
-              key: 'first-reservation',
-              width: 100,
-              render: (_, record) => {
-                if (record.date_of_first_reservation) {
-                  return moment(record.date_of_first_reservation).format(
-                    i18n.language === 'me' ? 'DD.MM.YYYY.' : 'YYYY-MM-DD'
-                  );
-                }
-                return '-';
-              },
-              ellipsis: true,
-            },
-            {
-              title: t('tableHeaders.lastReservationDate'),
-              dataIndex: 'date_of_last_reservation',
-              key: 'last-reservation',
-              width: 110,
-              render: (_, record) => {
-                if (record.date_of_last_reservation) {
-                  return moment(record.date_of_last_reservation).format(
-                    i18n.language === 'me' ? 'DD.MM.YYYY.' : 'YYYY-MM-DD'
-                  );
-                }
-                return '-';
-              },
-              ellipsis: true,
-            },
-            {
-              title: t('tableHeaders.remarks'),
-              dataIndex: 'remarks',
-              key: 'remarks',
-              width: 200,
-              ellipsis: true,
-              render: (_, record) => {
-                if (record.remarks) {
-                  return record.remarks;
-                }
-                return '-';
-              },
-            },
-            {
-              title: t('tableHeaders.actions'),
-              key: 'action',
-              align: 'center',
-              width: 100,
-              fixed: 'right',
-              render: (text, record) => (
-                <Space size="small">
-                  <Button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      if (!record?.user?.id) {
-                        message.error(
-                          'Ovaj klijent je seedovan i nema odgovarajućeg usera'
-                        );
-                        return;
-                      }
-                      handleEditClientClick(record.user.id);
-                    }}
-                    icon={<EditOutlined />}
-                  />
-                  <Button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      if (!record?.user?.id) {
-                        message.error(
-                          'Ovaj klijent je seedovan i nema odgovarajućeg usera'
-                        );
-                        return;
-                      }
-                      confirm({
-                        title: t('modals.confirmClientDelete'),
-                        icon: <ExclamationCircleOutlined />,
-                        content: t('modals.actionPermanentClient'),
-                        okType: 'danger',
-                        onOk() {
-                          return mutation.mutateAsync(
-                            record.user && record.user.id
-                          );
-                        },
-                        onCancel() {},
-                      });
-                    }}
-                    icon={<DeleteOutlined />}
-                    danger
-                  />
-                </Space>
-              ),
-            },
-          ]}
           dataSource={
             [].concat.apply(
               [],
               response?.pages.map((page) => page.data.data)
             ) || []
           }
+          showHeader={false}
           rowKey={(record) => record.id}
           pagination={false}
-          scroll={{ x: '100%', y: '60vh' }}
           size="small"
           onRow={(record, index) => {
             return {
@@ -294,18 +341,99 @@ export default function ClientsPage() {
                   : null,
             };
           }}
+          columns={[
+            {
+              title: t('tableHeaders.clientName'),
+              dataIndex: 'name',
+              key: 'name',
+              width: '100%',
+              render: (_, record) => {
+                return (
+                  <>
+                    <Descriptions
+                      className="small-screen-table-descriptions"
+                      bordered
+                      size="small"
+                      column={1}
+                      labelStyle={{ width: '50%', textAlign: 'right' }}
+                      contentStyle={{ color: 'grey' }}
+                    >
+                      <Descriptions.Item label={t('formLabels.name')}>
+                        {record.name}
+                      </Descriptions.Item>
+                      <Descriptions.Item label={t('formLabels.email')}>
+                        {record.email}
+                      </Descriptions.Item>
+                      <Descriptions.Item label={t('formLabels.phoneShort')}>
+                        {record.phone_no}
+                      </Descriptions.Item>
+                      <Descriptions.Item label={t('formLabels.IdDocShort')}>
+                        {record.identification_document_no}
+                      </Descriptions.Item>
+                      <Descriptions.Item
+                        label={t('tableHeaders.firstReservationDate')}
+                      >
+                        {record.date_of_first_reservation
+                          ? moment(record.date_of_first_reservation).format(
+                              i18n.language === 'me'
+                                ? 'DD.MM.YYYY.'
+                                : 'YYYY-MM-DD'
+                            )
+                          : '-'}
+                      </Descriptions.Item>
+                      <Descriptions.Item
+                        label={t('tableHeaders.lastReservationDate')}
+                      >
+                        {record.date_of_last_reservation
+                          ? moment(record.date_of_last_reservation).format(
+                              i18n.language === 'me'
+                                ? 'DD.MM.YYYY.'
+                                : 'YYYY-MM-DD'
+                            )
+                          : '-'}
+                      </Descriptions.Item>
+                    </Descriptions>
+                    <Space size="small" className="small-screen-table-buttons">
+                      <Button
+                        className="small-screen-table-button"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleEditClientClick(record.user.id);
+                        }}
+                        icon={<EditOutlined />}
+                      >
+                        Edit
+                      </Button>
+                      <Button
+                        className="small-screen-table-button"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          confirm({
+                            title: t('modals.confirmClientDelete'),
+                            icon: <ExclamationCircleOutlined />,
+                            content: t('modals.actionPermanentClient'),
+                            okType: 'danger',
+                            onOk() {
+                              return mutation.mutateAsync(
+                                record.user && record.user.id
+                              );
+                            },
+                            onCancel() {},
+                          });
+                        }}
+                        icon={<DeleteOutlined />}
+                        danger
+                      >
+                        Delete
+                      </Button>
+                    </Space>
+                  </>
+                );
+              },
+            },
+          ]}
         />
-        <div
-          style={{
-            position: 'absolute',
-            bottom: 0,
-            width: '100%',
-            textAlign: 'center',
-          }}
-        >
-          {isFetchingNextPage && <Spin size="small" />}
-        </div>
-      </Card>
+      )}
     </>
   );
 }
